cd907183fb77d699220ab5092b5516d8733801ca
ersan-arik <ersan.arik@gmail.com>
2019-05-05 12:07:30 +0300

    changed english variable

--- hardcode.js
+++ hardcode.js
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License")
 var http = require('http');
 var server = http.createServer(function (request, response) {
-  const language = 'Turkish';
+  const language = 'English';
   const API_KEY = '123-456-789';
   response.write(`Language: ${language}\n`);
   response.write(`API Key: ${API_KEY}\n`);
